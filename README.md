47691cbd2ea36e1d97ba56aa7574bb6fa9a0fc56
MSayib <sayib.roziq2@gmail.com>
2022-09-19 13:04:13 +0700

    Sample use case of wildcard

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - [x] Passing Data to Blade Components
 - [x] Layouting with Components
 - [x] Dealing with Custom Style/Js from Components
-- [ ] Request & Wildcard [Working on progress]
+- [x] Request & Wildcard
 - [ ] …
 
 This project was made only because im re-learning Laravel (newer Laravel version from my last working-projects) and documenting at the same time, incase im forgot what i’ve learnt before.
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,26 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('home')); //Min: PHP 7.4+
 Route::view('contact', 'contact');
 Route::view('about', 'about');
-Route::view('profile', 'profile');
+// Route::view('profile', 'profile');
+
+
+// Option 1:
+// Route::get('profile', function(Request $request) {
+//     $name = $request->nami; // baseURL/profile?nami=Sayib
+//     return view('profile', ['name' => $name]);
+// });
+
+// Option 2:
+Route::get('profile/{username}', function($username) {
+    return view('profile', ['name' => $username]); // baseURL/profile/Sayib
+});
+
+// Option 2.1: More binding
+// Route::get('profile/{username}/{posts}', function($username, $posts) {
+//     return view('profile', ['name' => $username, 'posts' => $posts]); // baseURL/profile/Sayib/posts
+// });
